c7e887d8d2f9104ca03e9f603afe75c70b00b032
kkkssh <sanghee660@gmail.com>
2023-09-22 09:37:28 +0900

    Spring day01

--- myspringex1/src/main/java/org/iclass/my/SangheeDao.java
+++ myspringex1/src/main/java/org/iclass/my/SangheeDao.java
@@ -3,10 +3,12 @@ package org.iclass.my;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Component
-@Setter 
+//@Setter 
+//@RequiredArgsConstructor		//@Value 로 int 값을 초기화 하기 위해 직접 작성
 public class SangheeDao {
 	
 	private final int count;
